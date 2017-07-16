7d399efd0f23e2583e32460a451576434c8c0ff8
Sally Bouley <sallybouley87@gmail.com>
2017-07-15 21:42:08 -0600

    some functions

--- some/core.js
+++ some/core.js
@@ -2,28 +2,38 @@
 // array are numbers greater than 10.
 
 function anyGreaterThan10 (input) {
-  return input;
+  return input.some(function(number){
+    return number > 10;
+  });
 };
 
 // Check to see if any of the strings in
 // the array is longer than 10 characters.
 
 function longWord (input) {
-  return input;
+  return input.some(function(string){
+    return string.length > 10;
+  });
 };
 
 // Check to see if any of the elements in
 // the matrix are true.
 
 function truePossibilities (input) {
-  return input;
+  return input.some(function(matrix){
+    return matrix.some(function(value){
+      return value === true;
+    });
+  });
 };
 
 // Check to see if 'Lost' is in
 // the phrase (using some).
 
 function lostCarcosa (input) {
-  return input;
+  return input.some(function(word){
+    return word.includes('Lost');
+  });
 };
 
 module.exports = {
